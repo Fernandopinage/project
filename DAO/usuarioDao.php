4ffddf4dbc947013859244e219db28a17ac05c70
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-10-25 21:39:23 -0300

    validação do login ok

--- DAO/usuarioDao.php
+++ DAO/usuarioDao.php
@@ -1,49 +1,49 @@
-<?php 
+<?php
 require_once '../DAO/Dao.php';
 require_once '../class/classUsuario.php';
 
 
-class UsuarioDao extends Dao{ // a classe UsuarioDao se extendendo da classe pai DAO
+class UsuarioDao extends Dao
+{ // a classe UsuarioDao se extendendo da classe pai DAO
 
+
+
+    public function validarUsuario($email, $pass)
+    {
+
+        $sql = "SELECT * FROM `usuarios` WHERE EMAIL_USUARIO = :nome  and  PASS_USUARIO = :pass";
+        $select = $this->con->prepare($sql);
+        $select->bindValue(':nome', $email);
+        $select->bindValue(':pass', base64_encode($pass));
+        $select->execute();
+
+        var_dump($sql);
+
+        if ($select->rowCount()) {
+
+
+            header('location:../telas/painel.php');
+        } else {
+
+
+            header('location:../telas/index.php');
+        }
+    }
 
     public function InsertUsuario(Usuario $Usuario)
     {
         $sql = "INSERT INTO `usuarios`(`ID_USUARIO`, `NOME_USUARIO`, `EMAIL_USUARIO`, `PASS_USUARIO`, `TELEFONE_USUARIO`, `CELULAR_USUARIO`)
          VALUES (null,:nome, :email, :pass, :telefone, :celular);";
         $insert = $this->con->prepare($sql);
-        $insert->bindValue(":nome" , $Usuario->getNome());
-        $insert->bindValue(":email" , $Usuario->getEmail());
-        $insert->bindValue(":telefone" , $Usuario->getTelefone());
-        $insert->bindValue(":celular" , $Usuario->getCelular());
-        $insert->bindValue(":pass" , $Usuario->getPassword());
+        $insert->bindValue(":nome", $Usuario->getNome());
+        $insert->bindValue(":email", $Usuario->getEmail());
+        $insert->bindValue(":telefone", $Usuario->getTelefone());
+        $insert->bindValue(":celular", $Usuario->getCelular());
+        $insert->bindValue(":pass", $Usuario->getPassword());
         $insert->execute();
-        if($insert -> rowCount() >0){
-            
-            header('location: ../telas/painel.php');
+        if ($insert->rowCount() > 0) {
 
+            header('location: ../telas/painel.php');
         }
     }
-
-    public function validarUsuario(Usuario $Usuario){
-
-       
-        
-        $sql = "SELECT `NOME_USUARIO`,`PASS_USUARIO` FROM `usuarios` WHERE NOME_USUARIO = :nome  and PASS_USUARIO = :pass";
-        $select = $this->con->prepare($sql);
-        $select->bindValue(":nome",$Usuario->getEmail());
-        $select->bindValue(":nome",base64_encode($Usuario->getPassword()));
-        $select->execute();
-        if ( $select->rowCount()) {
-            
-            
-            header('location:../PROJETO/telas/painel.php');
-            
-        } else {
-            
-            header('location:../PROJETO/index.php');
-    }
-
 }
-}
-
-?>
---
+++ telas/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once "../class/classUsuario.php";
+require_once "../DAO/usuarioDao.php";
+
+
+
+
+if(isset($_POST['enviar'])){
+
+   $email = $_POST['email'];
+   $pass = $_POST['pass'];
+
+   $Pessoa = new UsuarioDao();
+   $Pessoa->validarUsuario($email,$pass);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="login">
+        <div class="login-triangle"></div>
+
+        <h2 class="login-header">Login</h2>
+
+        <form class="login-container" action="index.php" method="POST">
+            <p><input type="email" placeholder="Email" name="email"></p>
+            <p><input type="password" placeholder="Password" name="pass"></p>
+            <p><input type="submit" name="enviar" value="Conecte-se"></p>
+        </form>
+    </div>
+</body>
+
+</html>
